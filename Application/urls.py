b14f74f90c96528e5d6e2ca5593c08f2060d9b70
xscrypto001 <ikinyapeter93@gmail.com>
2025-07-06 17:24:28 +0300

    fix: edit + add departments urls

--- Application/urls.py
+++ Application/urls.py
@@ -5,6 +5,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
+path('departments/', views.department_list, name='department_list'),
+    path('departments/add/', views.add_department, name='add_department'),
+    path('departments/<int:pk>/edit/', views.edit_department, name='edit_department'),
+    path('departments/<int:pk>/delete/', views.delete_department, name='delete_department'),
+
 path('', views.index, name='index'),
 path('employees/add/', views.add_employee, name='add_employee'),
 path('employees/', views.employee_list, name='employee_list'),
