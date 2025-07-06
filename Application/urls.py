8c0ef016ee0970e5d55899b530778146bbc79467
xscrypto001 <ikinyapeter93@gmail.com>
2025-07-06 17:13:35 +0300

    fix: edit + add employee urls

--- Application/urls.py
+++ Application/urls.py
@@ -6,6 +6,10 @@ from . import views
 
 urlpatterns = [
 path('', views.index, name='index'),
+path('employees/add/', views.add_employee, name='add_employee'),
+path('employees/', views.employee_list, name='employee_list'),
+    path('employees/<int:pk>/edit/', views.edit_employee, name='edit_employee'),
+
 path('logout/', LogoutView.as_view(), name='logout'),
      path('signup/', views.signup_view, name='register'),
     path('auth/', views.login_page, name='auth'),
