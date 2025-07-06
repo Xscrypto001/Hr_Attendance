adbee8cdcfba3faf2182ebb8c099c2dda9eacd63
xscrypto001 <ikinyapeter93@gmail.com>
2025-07-06 16:27:16 +0300

    fix: timezone err

--- Application/views.py
+++ Application/views.py
@@ -1,4 +1,5 @@
-# views.py
+from django.utils import timezone
+
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
